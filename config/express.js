44a944e5933112ef381299b15fb59248d44df0f0
RandelSouza <randelsouza88@gmail.com>
2019-07-09 01:02:36 -0300

    "insert dependency cookie-parser"

--- config/express.js
+++ config/express.js
@@ -1,2 +1,5 @@
 var express = require('express');
 var consign = require('consign');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var cookieParser = require('cookie-parser');
