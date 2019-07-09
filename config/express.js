3a15640a23c6b851a798fc12df93866941daa856
RandelSouza <randelsouza88@gmail.com>
2019-07-09 01:31:34 -0300

    "add express.js"

--- config/express.js
+++ config/express.js
@@ -3,3 +3,32 @@ var consign = require('consign');
 var bodyParser = require('body-parser');
 var session = require('express-session');
 var cookieParser = require('cookie-parser');
+
+module.exports = function(){
+    var app = express();
+
+    // Configurações
+    app.set('port', 3000);
+    app.set('view engine', 'ejs');
+    app.set('views', './app/views');
+
+    // Middleware
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended: true}));
+    app.use(express.static('./public'));
+    app.use(cookieParser());
+
+    app.use(session({
+        secret: 'keyboard cat',
+        resave: true,
+        saveUninitilized: true
+    }));
+
+    consign({cwd: 'app'})
+        .include('models')
+        .then('controllers')
+        .then('routes')
+        .into(app);
+
+    return app;
+};
