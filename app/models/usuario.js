9287d96d882c847084d401e13f83d8cd4a3b0280
RandelSouza <randelsouza88@gmail.com>
2019-07-09 23:43:13 -0300

    "add models usuario.js"

---
+++ app/models/usuario.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose');
+
+module.exports = function() {
+    var schema = mongoose.Schema({
+        usuario: {
+            type: String,
+            required: true
+        },
+        senha: {
+            type: String,
+            required: true
+        }
+    });
+    return mongoose.model('Usuario', schema)
+};
