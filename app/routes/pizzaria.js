41b607738c1308b2c2775eadeee693d6f537c68f
RandelSouza <randelsouza88@gmail.com>
2019-07-13 10:59:28 -0300

    "update files"

--- app/routes/pizzaria.js
+++ app/routes/pizzaria.js
@@ -2,5 +2,5 @@ module.exports = function(app) {
     var controller = app.controllers.pizzaria;
     app.get('/', controller.home);
     //app.post('/login', controller.validacao);
-    //app.get('/logout', controller.logout);
+    //app.get('/logout', controller.logout);sl
 }
