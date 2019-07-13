0680bbdc187ca9b6bcb295e03174788726c6ad62
RandelSouza <randelsouza88@gmail.com>
2019-07-13 10:49:04 -0300

    "update files"

--- app/routes/login.js
+++ app/routes/login.js
@@ -2,5 +2,5 @@ module.exports = function(app) {
     var controller = app.controllers.login;
     app.get('/login', controller.login);
     app.post('/login', controller.validacao);
-    app.get('/  logout', controller.logout);
+    app.get('/logout', controller.logout);
 }
