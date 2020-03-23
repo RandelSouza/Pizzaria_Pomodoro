632f90ed9356e10705d5c74e23503375e1dfaa76
RandelSouza <randelsouza88@gmail.com>
2020-03-23 18:19:09 +0000

    upate file

--- insert_users_exec.sh
+++ insert_users_exec.sh
@@ -2,4 +2,5 @@ mongo <<EOF
 use pizzaria  
 db.usuarios.insert( { login: "admin", senha: "2fb54875d166ab45a6a8b21f1631dec573a8cf07" } )  
 db.usuarios.insert( { login: "randel", senha: "2fb54875d166ab45a6a8b21f1631dec573a8cf07" } )  
+exit
 EOF  
