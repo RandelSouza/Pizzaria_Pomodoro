73d9dab4e078719d06b1665d37f08a773e13570a
RandelSouza <randelsouza88@gmail.com>
2020-03-31 15:21:41 -0300

    indenting file pizzaria.js

--- app/controllers/pizzaria.js
+++ app/controllers/pizzaria.js
@@ -1,64 +1,65 @@
 module.exports = (app) => {
-        var hash = require('object-hash');
-        var Pizza = app.models.pizza;
+    var hash = require('object-hash');
+    var Pizza = app.models.pizza;
 
-        var controller = {};
+    var controller = {};
 
-        controller.home = (req, res) => app.controllers.login.autoriza(req, res, () =>res.render('home'));
-        
-        controller.about_us = (req, res) => app.controllers.login.autoriza(req, res, () => res.render('sobre_nos')); 
-        
-        controller.adicionar_pizza_page = (req, res) => app.controllers.login.autoriza(req, res, () => res.render('cardapio_pizzas'));
-        
-        controller.pizzas = (req, res) => app.controllers.login.autoriza(req, res, () => {
-             Pizza.find((err, pizzas) => {
-				// console.log(pizzas);
-				if (err) return console.error(err);
-				res.json(pizzas);
-            });
-        });           
+    controller.home = (req, res) => app.controllers.login.autoriza(req, res, () => res.render('home'));
 
-        controller.adicionar_pizza = (req, res) => app.controllers.login.autoriza(req, res, () => {                        
-            var pizza = new Pizza({
-                nome: req.body.nome,
-                descricao: req.body.descricao,
-                preco: req.body.preco
-            });
+    controller.about_us = (req, res) => app.controllers.login.autoriza(req, res, () => res.render('sobre_nos'));
 
-            pizza.save((err, results) => {
-                console.log(results._id);
-            });
+    controller.adicionar_pizza_page = (req, res) => app.controllers.login.autoriza(req, res, () => res.render('cardapio_pizzas'));
 
-            res.redirect('/adicionar_pizza');
+    controller.pizzas = (req, res) => app.controllers.login.autoriza(req, res, () => {
+        Pizza.find((err, pizzas) => {
+            // console.log(pizzas);
+            if (err) return console.error(err);
+            res.json(pizzas);
         });
-                             
-        
-        controller.listar_pizzas = (req, res) => app.controllers.login.autoriza(req, res, () => res.render('listar_pizzas'));
-        
-        controller.atualizar_pizza = (req, res) => app.controllers.login.autoriza(req, res, () => {
-            var id = req.body.id;
-			var nome = req.body.nome;
-            var preco = req.body.preco;
-            var descricao = req.body.descricao;
-            
-            console.log("identificador: " + id);
+    });
 
-			Pizza.findByIdAndUpdate(id, {$set: { nome: nome, descricao: descricao, preco: preco} }, (err) => {
-				if (err) return console.error(err);
-				controller.pizzas(req, res);
-			});
-        });
-        
-
-        controller.deletar_pizza =  (req, res) => app.controllers.login.autoriza(req, res, () => {
-            var id = req.params.id;
-            Pizza.deleteOne({_id: id}, (err) => {
-                if (err) return console.error(err);
-				controller.pizzas(req, res);
-            });
+    controller.adicionar_pizza = (req, res) => app.controllers.login.autoriza(req, res, () => {
+        var pizza = new Pizza({
+            nome: req.body.nome,
+            descricao: req.body.descricao,
+            preco: req.body.preco
         });
 
-        controller.cardapio_bebidas = (req, res) => app.controllers.login.autoriza(req, res, () => res.render('cardapio_bebidas'));
+        pizza.save((err, results) => {
+            console.log(results._id);
+        });
+
+        res.redirect('/adicionar_pizza');
+    });
+
+
+    controller.listar_pizzas = (req, res) => app.controllers.login.autoriza(req, res, () => res.render('listar_pizzas'));
+
+    controller.atualizar_pizza = (req, res) => app.controllers.login.autoriza(req, res, () => {
+        var id = req.body.id;
+        var nome = req.body.nome;
+        var preco = req.body.preco;
+        var descricao = req.body.descricao;
+
+        console.log("identificador: " + id);
+
+        Pizza.findByIdAndUpdate(id, { $set: { nome: nome, descricao: descricao, preco: preco } }, (err) => {
+            if (err) return console.error(err);
+            controller.pizzas(req, res);
+        });
+    });
+
+
+    controller.deletar_pizza = (req, res) => app.controllers.login.autoriza(req, res, () => {
+        var id = req.params.id;
         
-        return controller;
+        Pizza.deleteOne({ _id: id }, (err) => {
+            if (err) return console.error(err);
+            controller.pizzas(req, res);
+        });
+    });
+
+    controller.cardapio_bebidas = (req, res) => app.controllers.login.autoriza(req, res, () => res.render('cardapio_bebidas'));
+
+    return controller;
 };
