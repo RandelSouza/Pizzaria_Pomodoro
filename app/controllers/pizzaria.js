4ba61c4aeca96600bd73c9558d4c029f9df3d200
RandelSouza <randelsouza88@gmail.com>
2020-04-02 13:33:19 -0300

    update files

--- app/controllers/pizzaria.js
+++ app/controllers/pizzaria.js
@@ -29,7 +29,7 @@ module.exports = (app) => {
             console.log(results._id);
         });
 
-        res.redirect('/adicionar_pizza');
+        //res.redirect('/adicionar_pizza');
     });
 
 
