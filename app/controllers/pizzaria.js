24de50cea2f2bc9f33d9fcc022afdf5ff5999ffd
RandelSouza <randelsouza88@gmail.com>
2021-01-01 18:43:00 -0100

    Modified from var to const in pizza controller

--- app/controllers/pizzaria.js
+++ app/controllers/pizzaria.js
@@ -1,7 +1,7 @@
 module.exports = (app) => {
     //var hash = require('object-hash');
-    var Pizza = app.models.pizza;
-    var controller = {};
+    const Pizza = app.models.pizza;
+    const controller = {};
     
     controller.home = (req, res) => app.controllers.login.autoriza(req, res, () => res.render('home'));
     controller.about_us = (req, res) => app.controllers.login.autoriza(req, res, () => res.render('sobre_nos'));    
